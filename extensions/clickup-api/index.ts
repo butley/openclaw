735adaf01c85c5cf87c94a826ad52e755d0cd7fa
Lucas Machado <lucasmpr@me.com>
2026-03-04 08:56:21 -0300

    Revert "feat(clickup-api): add ClickUp plugin with tasks + Docs v3 support"
    This reverts commit 61c65148ea620a5a3149addbb64aa445b32e6356.

--- extensions/clickup-api/index.ts
+++
@@ -1,9 +0,0 @@
-import type { AnyAgentTool, OpenClawPluginApi } from "../../src/plugins/types.js";
-import { createClickUpApiTool } from "./src/clickup-api-tool.js";
-
-export default function register(api: OpenClawPluginApi) {
-  const tool = createClickUpApiTool(api);
-  if (tool) {
-    api.registerTool(tool as unknown as AnyAgentTool);
-  }
-}
